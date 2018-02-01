96cf093abef8cceba66e6245e8be4f48a4380df9
hennaheinonen <henna.heinonen96@gmail.com>
2018-02-01 11:00:41 +0200

    TaskTicketPrice
    TaskTicketPrice

---
+++ TaskTicketPrice/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace TaskTicketPrice
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Ticket customer = new Ticket();
+
+            customer.isStudent = YesNo("Are you a student? Y/N?");
+            customer.isConscript = YesNo("Are you a conscript? Y/N?");
+            customer.isMtk = YesNo("Are you a MTK member? Y/N?");
+
+            Console.WriteLine("How old are you?");
+            string userInput = Console.ReadLine();
+            customer.Age = int.Parse(userInput);
+
+            customer.GetPrice();
+        }
+
+        public static bool YesNo(string message) 
+        {
+            Console.WriteLine(message);
+            string answer = Console.ReadLine().ToUpper();
+
+            if (answer == "Y")
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ TaskTicketPrice/Ticket.cs
@@ -0,0 +1,64 @@
+﻿using System;
+namespace TaskTicketPrice
+{
+    public class Ticket
+    {
+        //Fields
+        public int Age;
+        public float Price;
+        private bool _isStudent;
+        private bool _isMtk;
+        private bool _isConscript;
+        private int _discount;
+
+
+        public bool isStudent { get => _isStudent; set => _isStudent = value; }
+        public bool isConscript { get => _isConscript; set => _isConscript = value; }
+        public bool isMtk { get => _isMtk; set => _isMtk = value; }
+
+        //Constructor
+
+        public Ticket()
+        {
+            Age = 0;
+            Price = 16;
+            _discount = 0;
+
+        }
+        public Ticket(int age, int discount)
+        {
+            Age = age;
+            Price = 16;
+            _discount = discount;
+
+        }
+        //Methods 
+        public void GetPrice()
+        {
+            //ikäalennus
+            if (Age <= 7)
+                _discount = 100;
+
+
+            else if (Age >= 7 && Age <= 15)
+                _discount = 50;
+
+
+            else if (Age >= 65)
+                _discount = 50;
+
+            if (isConscript)
+                _discount = 50;
+
+            if (isStudent)
+                _discount = 45;
+
+            if (isMtk)
+                _discount = 15;
+
+            if (isMtk && isStudent)
+                _discount = 60;
+            Console.WriteLine($"Price = {Price} * {_discount}% = {Price *_discount/100:c2}");
+        }
+    }
+}
